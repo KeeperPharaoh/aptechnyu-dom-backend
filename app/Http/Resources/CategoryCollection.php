257660e8ef725ec59d02c6fff4eb710bf631ee41
Farkhad <farhad.begimbetov1>
2021-12-07 17:09:54 +0600

    add image

--- app/Http/Resources/CategoryCollection.php
+++ app/Http/Resources/CategoryCollection.php
@@ -6,18 +6,14 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class CategoryCollection extends ResourceCollection
 {
-    /**
-     * Transform the resource collection into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
+
     public function toArray($request)
     {
         return $this->collection->map(function ($item) {
             return [
                 'id'        => $item->id,
                 'title'     => $item->title,
+                'image'     => $item->image
             ];
         });
     }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,13 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Facades\Voyager;
 
-class Category extends Model
+class Category extends BaseModel
 {
     use HasFactory;
 
     public function products(){
         return $this->belongsToMany(Product::class,'category_products','category_id','product_id');
+    }
+
+
+    public function getImageAttribute($value)
+    {
+        if ($value != null){
+            $value = self::jsonDecode($value);
+            return Voyager::image($value);
+        }
+        return true;
     }
 }
 
